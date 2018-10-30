f2654851289084cc53b8560c73506f2ee015cd21
董新 <xin.dong@dewmobile.net>
2018-10-30 12:09:57 +0800

    init

---
+++ Example/Pods/Target Support Files/DGAlertKit/DGAlertKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DGAlertKit : NSObject
+@end
+@implementation PodsDummy_DGAlertKit
+@end
---
+++ Example/Pods/Target Support Files/DGAlertKit/DGAlertKit-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ADManager.h"
+#import "ADWebViewController.h"
+#import "AppDelegate+ADExt.h"
+#import "GTMBase64.h"
+#import "GTMDefines.h"
+
+FOUNDATION_EXPORT double DGAlertKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char DGAlertKitVersionString[];
+
